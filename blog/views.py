3bb6387b50cf43bc98fb9efbd61d86d3fa605456
suelensca10 <sscariott@ucs.br>
2019-10-15 20:15:35 -0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
     #A variável posts é o nome do queryset que faz esse filtro abaixo. Quando precisar pode se referenciar a ele por esse nome
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
