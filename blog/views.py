95712bef0aa08223734b6e21294aae843bb13061
suelensca10 <sscariott@ucs.br>
2019-10-15 21:50:18 -0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
+#views conectam modelos (banco de dados) com templates
+
+from django.shortcuts import redirect
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
@@ -12,3 +16,34 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    # a variável form recebe o form chamado PostForm, ou seja, está fazendo o caminho inverso do que
+    # vimos até agora. Aqui a informação vem da tela e vai pro banco
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        #edita o post q já existia
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
